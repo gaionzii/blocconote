cc990fbd13c2ceeb387b870b0f25d94909385a13
gaionzii <gaia@baldanzi.net>
2025-02-21 02:22:46 +0100

    creazione delle classi observer e subject che poi mi serviranno per la creazione del design pattern observer

--- main.cpp
+++ main.cpp
@@ -2,3 +2,24 @@
 Ogni nota è composta da titolo e testo, può essere bloccata per evitarne cancellazione/modifica e fa parte di una collezione
 specificata con nome. Può anche far parte di una collezione di note importanti oltre che di una collezione specifica. Usare
 Observer per indicare quante note stanno in una collezione. */
+
+#include <iostream>
+#include <string>
+#include <list>
+using namespace std;
+
+class Observer {
+public:
+    ~Observer() {};
+    virtual void update() = 0;
+};
+
+class Subject {
+private:
+    list<Observer*> obs;
+public:
+    virtual ~Subject() {};
+    virtual void addObserver(Observer* o)=0;
+    virtual void removeObserver(Observer* o)=0;
+    virtual void notifyObserver()=0;
+};
